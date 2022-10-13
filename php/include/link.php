87650971f94213bd4456f13a79bbd2505e2d5a43
eu-ny <106949433+eu-ny@users.noreply.github.com>
2022-10-13 15:57:34 +0900

    20221013

---
+++ php/include/link.php
@@ -0,0 +1,29 @@
+<!-- CSS -->
+<link rel="stylesheet" href="../assets/css/fonts.css" />
+<link rel="stylesheet" href="../assets/css/reset.css" />
+<link rel="stylesheet" href="../assets/css/common.css" />
+
+<link rel="stylesheet" href="../assets/css/header.css" />
+<link rel="stylesheet" href="../assets/css/login.css">
+<link rel="stylesheet" href="../assets/css/loginPopup.css">
+<link rel="stylesheet" href="../assets/css//agree.css">
+
+<link rel="stylesheet" href="../assets/css/notice.css">
+<link rel="stylesheet" href="../assets/css/Review.css">
+<link rel="stylesheet" href="../assets/css/ReviewView.css">
+<link rel="stylesheet" href="../assets/css/talk.css">
+
+<link rel="stylesheet" href="../assets/css/footer.css">
+
+<link rel="stylesheet" href="../assets/css/swiper.css">
+<!-- //CSS -->
+
+<!-- SEO -->
+<meta name="author" content="enny" />
+<meta name="description" content="PHP 사이트 만들기 입니다." />
+<meta name="keyword" content="PHP, 로그인, 회원가입, 사이트, 으니" />
+<meta name="robots" content="all" />
+<!-- //SEO -->
+
+<!-- icon -->
+<!-- //icon -->
