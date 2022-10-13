38c093369c03631c830f77b5101434e80d340c83
eu-ny <106949433+eu-ny@users.noreply.github.com>
2022-10-13 15:10:50 +0900

    20221013

---
+++ php/connect/connect.php
@@ -0,0 +1,9 @@
+<?php
+    // 기본 설정
+    $host = "localhost";
+    $user = "praise1109";
+    $pass = "leehaeun3727!";
+    $db = "praise1109";
+    $connect = new mysqli($host, $user, $pass, $db);
+    $connect -> set_charset("utf8");
+?>
