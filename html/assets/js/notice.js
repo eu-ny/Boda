42daa3664306840078bad78590c2442116dbe235
eu-ny <106949433+eu-ny@users.noreply.github.com>
2022-10-22 21:16:15 +0900

    20221022

--- html/assets/js/notice.js
+++ html/assets/js/notice.js
@@ -1,6 +1,6 @@
-const OpenBtn = document.querySelectorAll(".board__table table tbody tr td .open");
-const CloseBtn = document.querySelectorAll(".board__table table tbody tr td .close");
-const content = document.querySelectorAll(".board__table table tbody tr td.content");
+const OpenBtn = document.querySelectorAll(".notice__board .notice__table tbody tr td .open");
+const CloseBtn = document.querySelectorAll(".notice__board .notice__table tbody tr td .close");
+const content = document.querySelectorAll(".notice__board .notice__table tbody tr td.content");
 
 for(let i=0; i<content.length; i++){
     OpenBtn[i].addEventListener("click", () => {
@@ -16,19 +16,19 @@ for(let i=0; i<content.length; i++){
 }
 
 
-const OpenBtn2 = document.querySelectorAll(".board__table2 table tbody tr td .open");
-const CloseBtn2 = document.querySelectorAll(".board__table2 table tbody tr td .close");
-const content2 = document.querySelectorAll(".board__table2 table tbody tr td.content");
+// const OpenBtn2 = document.querySelectorAll(".notice__board2 .notice__table tbody tr td .open");
+// const CloseBtn2 = document.querySelectorAll(".notice__board2 .notice__table tbody tr td .close");
+// const content2 = document.querySelectorAll(".notice__board2 .notice__table tbody tr td.content");
 
-for(let i=0; i < content2.length; i++){
-    OpenBtn2[i].addEventListener("click", () => {
-        content2[i].classList.remove("blind");
-        OpenBtn2[i].classList.add("blind");
-        CloseBtn2[i].classList.remove("blind");
-    })
-    CloseBtn2[i].addEventListener("click", () => {
-        content2[i].classList.add("blind");
-        OpenBtn2[i].classList.remove("blind");
-        CloseBtn2[i].classList.add("blind");
-    })
-}
+// for(let i=0; i < content2.length; i++){
+//     OpenBtn2[i].addEventListener("click", () => {
+//         content2[i].classList.remove("blind");
+//         OpenBtn2[i].classList.add("blind");
+//         CloseBtn2[i].classList.remove("blind");
+//     })
+//     CloseBtn2[i].addEventListener("click", () => {
+//         content2[i].classList.add("blind");
+//         OpenBtn2[i].classList.remove("blind");
+//         CloseBtn2[i].classList.add("blind");
+//     })
+// }
